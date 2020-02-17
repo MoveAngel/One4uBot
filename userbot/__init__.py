79a6f7eead505f06b45c2197a6b3f7e6f7ab3602
MoveAngel <moveangel29@gmail.com>
2020-02-17 21:00:22 +0700

    Userbot: Fix issue wrong destination repo upstream
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -75,7 +75,7 @@ HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/mkaraniya/OpenUserBot.git")
+    "https://github.com/MoveAngel/One4uBot.git")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
