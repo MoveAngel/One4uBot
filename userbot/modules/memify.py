e665e6bdd08fd5a735b00787da9da53e918f9244
MoveAngel <moveangel29@gmail.com>
2020-02-17 14:22:34 +0700

    Userbot: Fix typo

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -1,4 +1,4 @@
-# Ported by MVaL
+# Ported by MvAL
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon import events
@@ -38,7 +38,7 @@ async def mim(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("`Syntax: reply to an image with .mms` 'text on top' ; 'text on bottom' ")
+       await event.edit("`Syntax: reply to an image with .mmf` 'text on top' ; 'text on bottom' ")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.media:
