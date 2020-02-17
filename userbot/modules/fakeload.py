e4b2681c97094164e203868777068fd11a9008c5
MoveAngel <moveangel29@gmail.com>
2020-02-17 14:31:53 +0700

    Userbot: Thanks to @GudMeong for explain my idea
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/modules/fakeload.py
+++ userbot/modules/fakeload.py
@@ -1,4 +1,6 @@
 #made by @DneZyeK
+#Port by MvAL
+
 import asyncio
 import re
 import time
@@ -6,11 +8,11 @@ from time import sleep
 from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
-@register(outgoing=True, pattern='^.fl(?: |$)(.*)')
+@register(outgoing=True, pattern="^.fl(?: |$)(.*)")
 async def typewriter(typew):
 	message = typew.pattern_match.group(1)
-	await typew.edit("`start loading...`")
-	sleep(1)
+	await typew.edit("`start loading makan tokek goreng!...`")
+	sleep(4)
 	await typew.edit("0%")
 	number = 1
 	await typew.edit(str(number) + "%   ▎")
@@ -312,11 +314,11 @@ async def typewriter(typew):
 	sleep(0.03)
 	await typew.edit(str(number) + "%   ████████████████▌")
 	sleep(1)
-	await typew.edit("`Done!`")
+	await typew.edit("Tokek Goreng Siap di Santap! Jangan lupa Menu penutup nya [DISINI](http://javbus.com)")
 	# I did it for two hours :D just ctrl+c - crtl+v
 
-
-
-
-
-
+CMD_HELP.update({
+    "fakeload":
+    ".fl\
+    \nUsage: Tokek Goreng Dan makanan penutup."
+})
