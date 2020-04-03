feab46fedbc73362d64656f6bac8afa7fa86e919
MoveAngel <moveangel29@gmail.com>
2020-04-03 17:00:10 +0700

    Updater: Add botlog when your bot success update
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -15,7 +15,7 @@ import sys
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from userbot import (CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, UPSTREAM_REPO_URL, UPSTREAM_REPO_BRANCH)
+from userbot import (BOTLOG, BOTLOG_CHATID, CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, UPSTREAM_REPO_URL, UPSTREAM_REPO_BRANCH)
 from userbot.events import register
 
 requirements_path = path.join(
@@ -84,6 +84,12 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             return repo.__del__()
         await event.edit('`Successfully Updated!\n'
                          'Restarting, please wait...`')
+
+        if BOTLOG:
+            await event.client.send_message(
+                BOTLOG_CHATID, "#UPDATE \n"
+                "Your One4uBot was successfully updated")
+
     else:
         await event.edit('`[HEROKU]:'
                          '\nPlease set up` **HEROKU_API_KEY** `variable.`'
@@ -99,6 +105,12 @@ async def update(event, repo, ups_rem, ac_br):
     await update_requirements()
     await event.edit('`Successfully Updated!\n'
                      'Bot is restarting... Wait for a second!`')
+
+    if BOTLOG:
+            await event.client.send_message(
+                BOTLOG_CHATID, "#UPDATE \n"
+                "Your One4uBot was successfully updated")
+
     # Spin a new instance of bot
     args = [sys.executable, "-m", "userbot"]
     execle(sys.executable, *args, environ)
