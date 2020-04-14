72715024e50430f7df3c3aaf333f1ada49b45bc4
MoveAngel <moveangel29@gmail.com>
2020-04-14 07:30:32 +0700

    userbot: update help
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -19,10 +19,10 @@ async def help(event):
         else:
             await event.edit("Please specify a valid module name.")
     else:
-        await event.edit("Please specify which module do you want help for !!\
-            \nUsage: .help <module name>")
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`  ||  "
-        await event.reply(string)
+            string += "`\t\t\t||\t\t\t "
+        await event.edit(f"{string}"
+                        "\n\nSpecify which module do you want help for !!\
+                        \n**Usage:** `.help` <module name>")
