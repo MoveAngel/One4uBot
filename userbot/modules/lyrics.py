4b726cbab7025a1865bc04cacea0abb85c438a7c
MoveAngel <moveangel29@gmail.com>
2020-04-29 21:30:45 +0700

    Lyrics: Update
    ~ implement currently playing song lyrics feature
    
    ~ fix derp if no found artist and song lyrics
    
    ~ lyrics: tell user to use Client Access Token instead of Client ID or Client Secret
    
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/modules/lyrics.py
+++ userbot/modules/lyrics.py
@@ -6,47 +6,44 @@
 
 import os
 import lyricsgenius
-import random
 
 from userbot.events import register
-from userbot import CMD_HELP, GENIUS
+from userbot import (CMD_HELP, GENIUS, lastfm, LASTFM_USERNAME)
+from pylast import User
+
+if GENIUS is not None:
+    genius = lyricsgenius.Genius(GENIUS)
 
 
-@register(outgoing=True, pattern="^.lyrics(?: |$)(.*)")
+@register(outgoing=True, pattern="^.lyrics (?:(now)|(.*) - (.*))")
 async def lyrics(lyric):
-    if r"-" in lyric.text:
-        pass
-    else:
-        await lyric.edit("`Error: please use '-' as divider for <artist> and <song>`\n"
-                         "eg: `Nicki Minaj - Super Bass`")
-        return
-
+    await lyric.edit("`Getting information...`")
     if GENIUS is None:
-        await lyric.edit(
-            "`Provide genius access token to config.py or Heroku Var first!`")
+        return await lyric.edit(
+            "`Provide genius access token to Heroku ConfigVars...`")
+    if lyric.pattern_match.group(1) == "now":
+        playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
+        if playing is None:
+            return await lyric.edit(
+                "`No information current lastfm scrobbling...`"
+            )
+        artist = playing.get_artist()
+        song = playing.get_title()
     else:
-        genius = lyricsgenius.Genius(GENIUS)
-        try:
-            args = lyric.text.split('.lyrics')[1].split('-')
-            artist = args[0].strip(' ')
-            song = args[1].strip(' ')
-        except Exception:
-            await lyric.edit("`What!?!? Please provide artist and song names`")
-            return
-
-    if len(args) < 1:
-        await lyric.edit("`Please provide artist and song names`")
-        return
-
+        artist = lyric.pattern_match.group(2)
+        song = lyric.pattern_match.group(3)
     await lyric.edit(f"`Searching lyrics for {artist} - {song}...`")
-
     try:
         songs = genius.search_song(song, artist)
     except TypeError:
-        songs = None
-
+        return await lyric.edit(
+            "`Error credentials for GENIUS_ACCESS_TOKEN."
+            "Use Client Access Token - click Generate Access Token "
+            "instead of Client ID or Client Secret "
+            "from`  https://genius.com/api-clients"
+        )
     if songs is None:
-        await lyric.edit(f"Song **{artist} - {song}** not found!")
+        await lyric.edit(f"`Song`  **{artist} - {song}**  `not found...`")
         return
     if len(songs.lyrics) > 4096:
         await lyric.edit("`Lyrics is too big, view the file to see it.`")
@@ -56,16 +53,19 @@ async def lyrics(lyric):
             lyric.chat_id,
             "lyrics.txt",
             reply_to=lyric.id,
-            )
-        os.remove("lyrics.txt")
+        )
+        return os.remove("lyrics.txt")
     else:
-        await lyric.edit(f"**Search query**: \n`{artist} - {song}`\n\n```{songs.lyrics}```")
-    return
+        return await lyric.edit(
+            f"**Search query**:\n`{artist}` - `{song}`"
+            f"\n\n```{songs.lyrics}```"
+        )
 
 
 CMD_HELP.update({
-        "lyrics":
-        ".lyrics <artist name> - <song name> \
-          \nUsage: Searching the lyrics to divided artist and song\n"
-          "__note__: **-** is neccessary when searching the lyrics to divided artist and song"
-    })
+    "lyrics":
+    ".lyrics **<artist name> - <song name>**"
+    "\nUsage: Get lyrics matched artist and song."
+    "\n\n.lyrics now"
+    "\nUsage: Get lyrics artist and song from current lastfm scrobbling."
+})
