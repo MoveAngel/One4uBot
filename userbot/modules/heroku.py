080133bcb58c336c82b497e3c1c1f917daf82739
MoveAngel <moveangel29@gmail.com>
2020-05-04 22:25:26 +0700

    heroku: remove useless space
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -184,7 +184,7 @@ async def dyno_usage(dyno):
                            f" -> `Dyno usage for`  **{app.name}**:\n"
                            f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
                            f"**|**  [`{AppPercentage}`**%**]"
-                           "\n\n-----------------------------------------------------------\n\n"
+                           "\n-------------------------------------------------------------\n"
                            " -> `Dyno hours quota remaining this month`:\n"
                            f"     •  `{hours}`**h**  `{minutes}`**m**  "
                            f"**|**  [`{percentage}`**%**]"
