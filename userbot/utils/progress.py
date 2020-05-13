f49a7e23f30b2a1b237d2c5d52e8e6513dcfdcd3
MoveAngel <moveangel29@gmail.com>
2020-05-13 20:04:59 +0700

    GoogleDrive: cancel download tg media if .gdabort called
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/utils/progress.py
+++ userbot/utils/progress.py
@@ -8,22 +8,29 @@ import time
 import math
 
 from .tools import humanbytes, time_formatter
+from .exceptions import CancelProcess
 
 
-async def progress(current, total, gdrive, start, prog_type, file_name=None):
+async def progress(
+    current, total, gdrive, start, prog_type,
+    file_name=None, is_cancelled=False
+):
     now = time.time()
     diff = now - start
+    if is_cancelled is True:
+        raise CancelProcess
+
     if round(diff % 10.00) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff)
         eta = round((total - current) / speed)
         if 'upload' in prog_type.lower():
-            status = 'Uploading...'
+            status = 'Uploading'
         elif 'download' in prog_type.lower():
-            status = 'Downloading...'
+            status = 'Downloading'
         else:
-            status = 'Unknown...'
+            status = 'Unknown'
         progress_str = "`{0}` | [{1}{2}] `{3}%`".format(
             status,
             ''.join(["■" for i in range(
