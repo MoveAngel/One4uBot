737a0fdd9b709d8a934329f097e3f66ee40d5aee
MoveAngel <moveangel29@gmail.com>
2020-05-13 16:54:41 +0700

    android: publish new command pixeldl
    Thanks to adekmaulana
    
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -5,12 +5,18 @@
 #
 """ Userbot module containing commands related to android"""
 
+import asyncio
 import re
+import os
+import time
+import math
+
 from requests import get
 from bs4 import BeautifulSoup
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
+from userbot.utils import chrome, humanbytes, time_formatter, md5, human_to_bytes
 
 GITHUB = 'https://github.com'
 DEVICES_DATA = 'https://raw.githubusercontent.com/wulan17/' \
@@ -103,6 +109,114 @@ async def codename_info(request):
     await request.edit(reply)
 
 
+@register(outgoing=True, pattern="^.pixeldl(?: |$)(.*)")
+async def download_api(dl):
+    await dl.edit("`Collecting information...`")
+    URL = dl.pattern_match.group(1)
+    URL_MSG = await dl.get_reply_message()
+    if URL:
+        pass
+    elif URL_MSG:
+        URL = URL_MSG.text
+    else:
+        await dl.edit("`Empty information...`")
+        return
+    if not re.findall(r'\bhttps?://download.*pixelexperience.*\.org\S+', URL):
+        await dl.edit("`Invalid information...`")
+        return
+    driver = await chrome()
+    await dl.edit("`Getting information...`")
+    driver.get(URL)
+    error = driver.find_elements_by_class_name("swal2-content")
+    if len(error) > 0:
+        if error[0].text == "File Not Found.":
+            await dl.edit(f"`FileNotFoundError`: {URL} is not found.")
+            return
+    datas = driver.find_elements_by_class_name('download__meta')
+    """ - enumerate data to make sure we download the matched version - """
+    md5_origin = None
+    i = None
+    for index, value in enumerate(datas):
+        for data in value.text.split("\n"):
+            if data.startswith("MD5"):
+                md5_origin = data.split(':')[1].strip()
+                i = index
+                break
+        if md5_origin is not None and i is not None:
+            break
+    if md5_origin is None and i is None:
+        await dl.edit("`There is no match version available...`")
+    if URL.endswith('/'):
+        file_name = URL.split("/")[-2]
+    else:
+        file_name = URL.split("/")[-1]
+    file_path = TEMP_DOWNLOAD_DIRECTORY + file_name
+    download = driver.find_elements_by_class_name("download__btn")[i]
+    download.click()
+    await dl.edit("`Starting download...`")
+    file_size = human_to_bytes(download.text.split(None, 3)[-1].strip('()'))
+    display_message = None
+    complete = False
+    start = time.time()
+    while complete is False:
+        if os.path.isfile(file_path + '.crdownload'):
+            try:
+                downloaded = os.stat(file_path + '.crdownload').st_size
+                status = "Downloading"
+            except OSError:  # Rare case
+                await asyncio.sleep(1)
+                continue
+        elif os.path.isfile(file_path):
+            downloaded = os.stat(file_path).st_size
+            file_size = downloaded
+            status = "Checking"
+        else:
+            await asyncio.sleep(0.3)
+            continue
+        diff = time.time() - start
+        percentage = downloaded / file_size * 100
+        speed = round(downloaded / diff, 2)
+        eta = round((file_size - downloaded) / speed)
+        prog_str = "`{0}` | [{1}{2}] `{3}%`".format(
+            status,
+            "".join(["▮" for i in range(
+                    math.floor(percentage / 10))]),
+            "".join(["▯" for i in range(
+                    10 - math.floor(percentage / 10))]),
+            round(percentage, 2))
+        current_message = (
+            "`[DOWNLOAD]`\n\n"
+            f"`{file_name}`\n"
+            f"`Status`\n{prog_str}\n"
+            f"`{humanbytes(downloaded)} of {humanbytes(file_size)}"
+            f" @ {humanbytes(speed)}`\n"
+            f"`ETA` -> {time_formatter(eta)}"
+        )
+        if round(diff % 10.00) == 0 and display_message != current_message or (
+          downloaded == file_size):
+            await dl.edit(current_message)
+            display_message = current_message
+        if downloaded == file_size:
+            if not os.path.isfile(file_path):  # Rare case
+                await asyncio.sleep(1)
+                continue
+            MD5 = await md5(file_path)
+            if md5_origin == MD5:
+                complete = True
+            else:
+                await dl.edit("`Download corrupt...`")
+                os.remove(file_path)
+                driver.quit()
+                return
+    await dl.respond(
+        f"`{file_name}`\n\n"
+        f"Successfully downloaded to `{file_path}`."
+    )
+    await dl.delete()
+    driver.quit()
+    return
+
+
 @register(outgoing=True, pattern=r"^.specs(?: |)([\S]*)(?: |)([\s\S]*)")
 async def devices_specifications(request):
     """ Mobile devices specifications """
@@ -190,6 +304,8 @@ CMD_HELP.update({
 \nUsage: Get info about android device codename or model.\
 \n\n.codename <brand> <device>\
 \nUsage: Search for android device codename.\
+\n\n.pixeldl **<download.pixelexperience.org>**\
+\nUsage: Download pixel experience ROM into your userbot server.\
 \n\n.specs <brand> <device>\
 \nUsage: Get device specifications info.\
 \n\n.twrp <codename>\
