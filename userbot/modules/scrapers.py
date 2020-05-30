a28d90fe825e212694a4e405c1a36a2c29adff93
MoveAngel <moveangel@minaproject.tech>
2020-05-30 22:05:12 +0700

    scrapers: import new command getmusic
    Thanks to aidilaryanto
    
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -36,16 +36,19 @@ from youtube_dl.utils import (DownloadError, ContentTooShortError,
                               MaxDownloadsReached, PostProcessingError,
                               UnavailableVideoError, XAttrMetadataError)
 from asyncio import sleep
-from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID, YOUTUBE_API_KEY, CHROME_DRIVER, GOOGLE_CHROME_BIN
+from userbot import bot, CMD_HELP, BOTLOG, BOTLOG_CHATID, YOUTUBE_API_KEY, CHROME_DRIVER, GOOGLE_CHROME_BIN
 from userbot.events import register
+from telethon import events
 from telethon.tl.types import DocumentAttributeAudio
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.utils import progress, humanbytes, time_formatter
 from userbot.utils.google_images_download import googleimagesdownload
 
 CARBONLANG = "auto"
 TTS_LANG = "en"
 TRT_LANG = "en"
-TEMP_DOWNLOAD_DIRECTORY = "/root/userbot/.bin"
+TEMP_DOWNLOAD_DIRECTORY = "/root/One4uBot/.bin"
 
 
 @register(outgoing=True, pattern="^.crblang (.*)")
@@ -687,6 +690,90 @@ def deEmojify(inputString):
     return get_emoji_regexp().sub(u'', inputString)
 
 
+@register(outgoing=True, pattern="^.netease(?: |$)(.*)")
+async def WooMai(netase):
+    if netase.fwd_from:
+        return
+    song = netase.pattern_match.group(1)
+    chat = "@WooMaiBot"
+    link = f"/netease {song}"
+    await netase.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await netase.edit("`Downloading...Please wait`")
+          try:
+              msg = await conv.send_message(link)
+              response = await conv.get_response()
+              respond = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await netase.reply("```Please unblock @WooMaiBot and try again```")
+              return
+          await netase.edit("`Sending Your Music...`")
+          await asyncio.sleep(3)
+          await bot.send_file(netase.chat_id, respond)
+    await netase.client.delete_messages(conv.chat_id,
+                                       [msg.id, response.id, respond.id])
+    await netase.delete()
+
+
+@register(outgoing=True, pattern="^.sdd(?: |$)(.*)")
+async def DeezLoader(Deezlod):
+    if Deezlod.fwd_from:
+        return
+    d_link = Deezlod.pattern_match.group(1)
+    if ".com" not in d_link:
+        await Deezlod.edit("` I need a link to download something pro.`**(._.)**")
+    else:
+        await Deezlod.edit("**Initiating Download!**")
+    chat = "@DeezLoadBot"
+    async with bot.conversation(chat) as conv:
+          try:
+              msg_start = await conv.send_message("/start")
+              response = await conv.get_response()
+              r = await conv.get_response()
+              msg = await conv.send_message(d_link)
+              details = await conv.get_response()
+              song = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await Deezlod.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
+              return
+          await bot.send_file(Deezlod.chat_id, song, caption=details.text)
+          await Deezlod.client.delete_messages(conv.chat_id,
+                                             [msg_start.id, response.id, r.id, msg.id, details.id, song.id])
+          await Deezlod.delete()
+
+
+@register(outgoing=True, pattern="^.smd(?: |$)(.*)")
+async def SpoMusDown(TifyDown):
+    if TifyDown.fwd_from:
+        return
+    link = TifyDown.pattern_match.group(1)
+    chat = "@SpotifyMusicDownloaderBot"
+    await TifyDown.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await TifyDown.edit("`Downloading music taking some times,  Stay Tuned.....`")
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              msg = await bot.send_message(chat, link)
+              respond = await response
+              res = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              r = await res
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await TifyDown.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
+              return
+          await bot.forward_messages(TifyDown.chat_id, respond.message)
+    await TifyDown.client.delete_messages(conv.chat_id,
+                                       [msg.id, r.id, respond.id])
+    await TifyDown.delete()
+
+
 CMD_HELP.update({
     'img':
     '.img <search_query>\
@@ -729,4 +816,13 @@ CMD_HELP.update({
     'rip':
     '.ripaudio <url> or ripvideo <url>\
         \nUsage: Download videos and songs from YouTube (and [many other sites](https://ytdl-org.github.io/youtube-dl/supportedsites.html)).'
+})
+CMD_HELP.update({
+    "getmusic":
+    ".netease <Artist - Song Title>\
+    \nUsage: Download music with @WooMaiBot\
+    \n\n.sdd <Spotify/Deezer Link>\
+    \nUsage: Download music from Spotify or Deezer\
+    \n\n.smd <Artist - Song Title>\
+    \nUsage: Download music from Spotify"
 })
