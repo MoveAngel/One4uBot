99bbc40346407026cb2506e342ebcc5a2121e859
Mr.Miss <keselekpermen69@gmail.com>
2020-06-07 19:00:14 +0700

    nhentai: cleanup bot chat when completed
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/nhentai.py
+++ userbot/modules/nhentai.py
@@ -12,28 +12,36 @@ from userbot import bot, CMD_HELP
 from userbot.events import register
 
 @register(outgoing=True, pattern="^.nhentai(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
+async def _(hentai):
+    if hentai.fwd_from:
         return
-    link = event.pattern_match.group(1) 
+    link = hentai.pattern_match.group(1)
+    if not link:
+        return await hentai.edit("`I can't search nothing`")
     chat = "@nHentaiBot"
-    await event.edit("```Processing```")
+    await hentai.edit("```Processing```")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=424466890))
-              await bot.send_message(chat, link)
-              response = await response 
+              msg = await bot.send_message(chat, link)
+              response = await response
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @nHentaiBot and try again```")
+              await hentai.reply("```Please unblock @nHentaiBot and try again```")
               return
           if response.text.startswith("**Sorry I couldn't get manga from**"):
-             await event.edit("```I think this is not the right link```")
-          else: 
-             await event.delete()   
-             await bot.send_message(event.chat_id, response.message)
+             await hentai.edit("```I think this is not the right link```")
+          else:
+             await hentai.delete()
+             await bot.send_message(hentai.chat_id, response.message)
+             await bot.send_read_acknowledge(hentai.chat_id)
+             """ - cleanup chat after completed - """
+             await hentai.client.delete_messages(conv.chat_id,
+                                                [msg.id, response.id])
 
 CMD_HELP.update({
     "nhentai": 
     ".nhentai <link / code> \
-        \nUsage: view nhentai in telegra.ph XD\n"
+        \nUsage: view nhentai in telegra.ph\n"
     })
