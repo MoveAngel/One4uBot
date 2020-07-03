a72406bb19761e60fb67429bfd38968037870049
KenHV <kenhv@null.net>
2020-07-04 01:49:25 +0700

    torrent: improvements
    Change-Id: Iba19c36a6d28aa0cbf4021cb15363bcb75c64482
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/torrent.py
+++ userbot/modules/torrent.py
@@ -31,6 +31,9 @@ async def gengkapak(e):
         except:
             break
 
+    if not listdata:
+        return await e.edit("`Error: No results found`")
+
     tsfileloc = f"{TEMP_DOWNLOAD_DIRECTORY}/{query}.txt"
     with open(tsfileloc, "w+", encoding="utf8") as out_file:
         out_file.write(str(listdata))
