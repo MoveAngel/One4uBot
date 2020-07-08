d841105b2d1f8348d7a0d7b342e18bb20d19e01f
Angga <angga@linuxmail.org>
2020-07-08 23:22:01 +0700

    covid: change data source and improve output
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -14,18 +14,18 @@ from userbot.events import register
 async def corona(event):
     await event.edit("`Processing...`")
     country = event.pattern_match.group(1)
-    covid = Covid()
+    covid = Covid(source="worldometers")
     country_data = covid.get_status_by_country_name(country)
     if country_data:
         output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
         output_text += f"`Active      : {country_data['active']}`\n"
         output_text += f"`Deaths      : {country_data['deaths']}`\n"
         output_text += f"`Recovered   : {country_data['recovered']}`\n"
-        output_text += (
-            "`Last update : "
-            f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
-        )
-        output_text += f"Data provided by [Johns Hopkins University](https://j.mp/2xf6oxF)"
+        output_text += f"`New Cases   : {country_data['new_cases']}`\n"
+        output_text += f"`New Deaths  : {country_data['new_deaths']}`\n"
+        output_text += f"`Critical    : {country_data['critical']}`\n"
+        output_text += f"`Total Tests : {country_data['total_tests']}`\n\n"
+        output_text += f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
     else:
         output_text = "No information yet about this country!"
     await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
