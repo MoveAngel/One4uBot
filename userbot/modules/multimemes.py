74ae1e925be8231261421d86462cfdf231885329
MoveAngel <moveangel@minaproject.tech>
2020-07-15 22:58:54 +0700

    modules: merge sangmata to multimemes module
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/multimemes.py
+++ userbot/modules/multimemes.py
@@ -536,6 +536,37 @@ async def fryerrr(fry):
     return os.remove(downloaded_file_name)
 
 
+@register(outgoing=True, pattern="^.sg(?: |$)(.*)")
+async def lastname(steal):
+    if steal.fwd_from:
+        return 
+    if not steal.reply_to_msg_id:
+       await steal.edit("```Reply to any user message.```")
+       return
+    reply_message = await steal.get_reply_message() 
+    if not reply_message.text:
+       await steal.edit("```reply to text message```")
+       return
+    chat = "@SangMataInfo_bot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await steal.edit("```Reply to actual users message.```")
+       return
+    await steal.edit("```Sit tight while I steal some data from NASA```")
+    async with bot.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await bot.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await steal.reply("```Please unblock @sangmatainfo_bot and try again```")
+              return
+          if response.text.startswith("Forward"):
+             await steal.edit("```can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await steal.edit(f"{response.message.message}")
+
+
 @register(outgoing=True, pattern="^.waifu(?: |$)(.*)")
 async def waifu(animu):
     text = animu.pattern_match.group(1)
@@ -589,6 +620,14 @@ CMD_HELP.update({
             "\nUsage: deepfry image/sticker from the reply."
             "\n@image_deepfrybot"
     })
+
+
+CMD_HELP.update({
+        "sangmata": 
+        ".sg \
+          \nUsage: Steal ur or friend name."
+    })
+
 
 CMD_HELP.update({
         "waifu": 
