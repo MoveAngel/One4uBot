2f022d7b0a8446b5106e08869f8783f4372f7371
zainarbani <zaintsyariev@gmail.com>
2020-07-16 18:10:00 +0700

    multimemes: mmf: Fix broken path
    * Also cleanup
    
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- userbot/modules/multimemes.py
+++ userbot/modules/multimemes.py
@@ -72,9 +72,6 @@ if 1 == 1:
                                           "default_username_color": "#b48bf2"})
 
 
-THUMB_IMAGE_PATH = "./thumb_image.jpg"
-
-
 EMOJI_PATTERN = re.compile(
     "["
     "\U0001F1E0-\U0001F1FF"  # flags (iOS)
@@ -261,56 +258,53 @@ def get_markdown(reply):
 @register(outgoing=True, pattern="^.mmf(?: |$)(.*)")
 async def mim(event):
     if event.fwd_from:
-        return 
+        return
     if not event.reply_to_msg_id:
-       await event.edit("`Syntax: reply to an image with .mmf` 'text on top' ; 'text on bottom' ")
-       return
-    reply_message = await event.get_reply_message() 
+        await event.edit(
+            "`Syntax: reply to an image with .mmf` 'text on top' ; 'text on bottom' "
+        )
+        return
+
+    reply_message = await event.get_reply_message()
     if not reply_message.media:
-       await event.edit("```reply to a image/sticker/gif```")
-       return
-    chat = "@MemeAutobot"
-    sender = reply_message.sender
-    file_ext_ns_ion = "@memetime.png"
-    file = await bot.download_file(reply_message.media)
-    uploaded_gif = None
+        await event.edit("```reply to a image/sticker/gif```")
+        return
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
-       return
+        await event.edit("```Reply to actual users message.```")
+        return
     else:
-     await event.edit("```Transfiguration Time! Mwahaha Memifying this image! (」ﾟﾛﾟ)｣ ```")
-     await asyncio.sleep(5)
-    
-    async with bot.conversation("@MemeAutobot") as bot_conv:
-          try:
+        await event.edit(
+            "```Transfiguration Time! Mwahaha Memifying this image! (」ﾟﾛﾟ)｣ ```"
+        )
+
+    chat = "@MemeAutobot"
+    async with bot.conversation(chat) as bot_conv:
+        try:
             memeVar = event.pattern_match.group(1)
             await silently_send_message(bot_conv, "/start")
             await asyncio.sleep(1)
             await silently_send_message(bot_conv, memeVar)
             await bot.send_file(chat, reply_message.media)
             response = await bot_conv.get_response()
-          except YouBlockedUserError: 
-              await event.reply("```Please unblock @MemeAutobot and try again```")
-              return
-          if response.text.startswith("Forward"):
-              await event.edit("```can you kindly disable your forward privacy settings for good, Nibba?```")
-          if "Okay..." in response.text:
-            await event.edit("```🛑 🤨 NANI?! This is not an image! This will take sum tym to convert to image... UwU 🧐 🛑```")
-            thumb = None
-            if os.path.exists(THUMB_IMAGE_PATH):
-                thumb = THUMB_IMAGE_PATH
+        except YouBlockedUserError:
+            await event.reply("```Please unblock @MemeAutobot and try again```")
+            return
+        if response.text.startswith("Forward"):
+            await event.edit(
+                "```can you kindly disable your forward privacy settings for good, Nibba?```"
+            )
+        if "Okay..." in response.text:
+            await event.edit(
+                "```🛑 🤨 NANI?! This is not an image! This will take sum tym to convert to image... UwU 🧐 🛑```"
+            )
             input_str = event.pattern_match.group(1)
-            if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-                os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
             if event.reply_to_msg_id:
                 file_name = "meme.png"
                 reply_message = await event.get_reply_message()
-                to_download_directory = TEMP_DOWNLOAD_DIRECTORY
-                downloaded_file_name = os.path.join(to_download_directory, file_name)
+                downloaded_file_name = os.path.join(TEMP_DOWNLOAD_DIRECTORY, file_name)
                 downloaded_file_name = await bot.download_media(
-                    reply_message,
-                    downloaded_file_name,
-                    )
+                    reply_message, downloaded_file_name,
+                )
                 if os.path.exists(downloaded_file_name):
                     await bot.send_file(
                         chat,
@@ -318,33 +312,32 @@ async def mim(event):
                         force_document=False,
                         supports_streaming=False,
                         allow_cache=False,
-                        thumb=thumb,
-                        )
+                    )
                     os.remove(downloaded_file_name)
                 else:
                     await event.edit("File Not Found {}".format(input_str))
             response = await bot_conv.get_response()
-            the_download_directory = TEMP_DOWNLOAD_DIRECTORY
             files_name = "memes.webp"
-            download_file_name = os.path.join(the_download_directory, files_name)
+            download_file_name = os.path.join(TEMP_DOWNLOAD_DIRECTORY, files_name)
             await bot.download_media(
-                response.media,
-                download_file_name,
-                )
-            requires_file_name = TEMP_DOWNLOAD_DIRECTORY + "memes.webp"
+                response.media, download_file_name,
+            )
             await bot.send_file(  # pylint:disable=E0602
                 event.chat_id,
-                requires_file_name,
+                download_file_name,
                 supports_streaming=False,
                 caption="Memifyed",
             )
             await event.delete()
-            #await bot.send_message(event.chat_id, "`☠️☠️Ah Shit... Here we go Again!🔥🔥`")
-          elif not is_message_image(reply_message):
-            await event.edit("Invalid message type. Plz choose right message type u NIBBA.")
+        elif not is_message_image(reply_message):
+            await event.edit(
+                "Invalid message type. Plz choose right message type u NIBBA."
+            )
             return
-          else: 
-               await bot.send_file(event.chat_id, response.media)
+        else:
+            await bot.send_file(event.chat_id, response.media)
+            await event.delete()
+
 
 def is_message_image(message):
     if message.media:
