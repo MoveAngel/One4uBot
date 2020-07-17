081c1c01afb26f068e86fbde4743f6edea78f814
MoveAngel <moveangel@minaproject.tech>
2020-07-17 17:17:38 +0000

    pylint: auto fixes

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -18,16 +18,15 @@ async def corona(event):
     try:
         country_data = covid.get_status_by_country_name(country)
         output_text = (
-            f"`Confirmed   : {country_data['confirmed']}`\n"
-            + f"`Active      : {country_data['active']}`\n"
-            + f"`Deaths      : {country_data['deaths']}`\n"
-            + f"`Recovered   : {country_data['recovered']}`\n\n"
-            + f"`New Cases   : {country_data['new_cases']}`\n"
-            + f"`New Deaths  : {country_data['new_deaths']}`\n"
-            + f"`Critical    : {country_data['critical']}`\n"
-            + f"`Total Tests : {country_data['total_tests']}`\n\n"
-            + f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
-        )
+            f"`Confirmed   : {country_data['confirmed']}`\n" +
+            f"`Active      : {country_data['active']}`\n" +
+            f"`Deaths      : {country_data['deaths']}`\n" +
+            f"`Recovered   : {country_data['recovered']}`\n\n" +
+            f"`New Cases   : {country_data['new_cases']}`\n" +
+            f"`New Deaths  : {country_data['new_deaths']}`\n" +
+            f"`Critical    : {country_data['critical']}`\n" +
+            f"`Total Tests : {country_data['total_tests']}`\n\n" +
+            f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})")
         await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
     except ValueError:
         await event.edit(
