a5ab519016de843de76a264845e3fcd96a19ec9c
zainarbani <zaintsyariev@gmail.com>
2020-07-18 00:16:44 +0700

    covid: Handle exception
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -15,20 +15,24 @@ async def corona(event):
     await event.edit("`Processing...`")
     country = event.pattern_match.group(1)
     covid = Covid(source="worldometers")
-    country_data = covid.get_status_by_country_name(country)
-    if country_data:
-        output_text = f"`Confirmed   : {country_data['confirmed']}`\n"
-        output_text += f"`Active      : {country_data['active']}`\n"
-        output_text += f"`Deaths      : {country_data['deaths']}`\n"
-        output_text += f"`Recovered   : {country_data['recovered']}`\n\n"
-        output_text += f"`New Cases   : {country_data['new_cases']}`\n"
-        output_text += f"`New Deaths  : {country_data['new_deaths']}`\n"
-        output_text += f"`Critical    : {country_data['critical']}`\n"
-        output_text += f"`Total Tests : {country_data['total_tests']}`\n\n"
-        output_text += f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
-    else:
-        output_text = "No information yet about this country!"
-    await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
+    try:
+        country_data = covid.get_status_by_country_name(country)
+        output_text = (
+            f"`Confirmed   : {country_data['confirmed']}`\n"
+            + f"`Active      : {country_data['active']}`\n"
+            + f"`Deaths      : {country_data['deaths']}`\n"
+            + f"`Recovered   : {country_data['recovered']}`\n\n"
+            + f"`New Cases   : {country_data['new_cases']}`\n"
+            + f"`New Deaths  : {country_data['new_deaths']}`\n"
+            + f"`Critical    : {country_data['critical']}`\n"
+            + f"`Total Tests : {country_data['total_tests']}`\n\n"
+            + f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
+        )
+        await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
+    except ValueError:
+        await event.edit(
+            f"No information found for: {country}!\nCheck your spelling and try again."
+        )
 
 
 CMD_HELP.update({
