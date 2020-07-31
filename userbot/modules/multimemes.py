e7f6523d1a3b56aa0e9212364c6a54b6fb16d537
aidilaryanto <aidilaryanto18@gmail.com>
2020-07-31 23:24:48 +0800

    multimemes: sangmata: use send message instead of forward
    • squash change from:
        1. sangmata: change method to search by id [aidilaryanto/ProjectDils@316fbcd]
        2. sangmata: cleanup message after complete [aidilaryanto/ProjectDils@6078437]
    
    Co-authored-by: aidilaryanto <aidilaryanto18@gmail.com>
    Co-authored-by: Angga <angga@linuxmail.org>
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/multimemes.py
+++ userbot/modules/multimemes.py
@@ -539,31 +539,34 @@ async def lastname(steal):
     if not steal.reply_to_msg_id:
         await steal.edit("```Reply to any user message.```")
         return
-    reply_message = await steal.get_reply_message()
-    if not reply_message.text:
-        await steal.edit("```reply to text message```")
-        return
+    message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
-    reply_message.sender
-    if reply_message.sender.bot:
+    user_id = message.sender.id
+    id = f"/search_id {user_id}"
+    if message.sender.bot:
         await steal.edit("```Reply to actual users message.```")
         return
     await steal.edit("```Sit tight while I steal some data from NASA```")
     async with bot.conversation(chat) as conv:
         try:
-            response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=461843263))
-            await bot.forward_messages(chat, reply_message)
-            response = await response
+            msg = await conv.send_message(id)
+            r = await conv.get_response()
+            response = await conv.get_response()
         except YouBlockedUserError:
             await steal.reply("```Please unblock @sangmatainfo_bot and try again```")
             return
-        if response.text.startswith("Forward"):
-            await steal.edit("```can you kindly disable your forward privacy settings for good?```")
+        if response.text.startswith("No records"):
+            await steal.edit("```No records found for this user```")
+            await steal.client.delete_messages(
+                conv.chat_id,
+                [msg.id, r.id, response.id])
+            return
         else:
-            await steal.edit(f"{response.message.message}")
+            respond = await conv.get_response()
+            await steal.edit(f"{response.message}")
+        await steal.client.delete_messages(
+            conv.chat_id,
+            [msg.id, r.id, response.id, respond.id])
 
 
 @register(outgoing=True, pattern="^.waifu(?: |$)(.*)")
