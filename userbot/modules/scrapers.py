33af1ce0bb0e37d8c8963c1dea80e746b093eb3c
MoveAngel <moveangel@minaproject.tech>
2020-08-02 06:56:54 +0700

    scrapers: img: increase limit send file img to 8
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -133,7 +133,7 @@ async def img_sampler(event):
         lim = lim.replace("lim=", "")
         query = query.replace("lim=" + lim[0], "")
     except IndexError:
-        lim = 7
+        lim = 8
     response = googleimagesdownload()
 
     # creating list of arguments
