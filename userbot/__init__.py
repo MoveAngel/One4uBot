4d03fa3e36cf7139db653dd6f3999dcc8bda7aa3
MoveAngel <moveangel@minaproject.tech>
2020-08-29 06:51:34 +0700

    One4uBot: Bump version v3.7
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -137,7 +137,7 @@ COUNTRY = str(os.environ.get("COUNTRY") or "")
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER") or 1)
 
 # Version of One4uBot
-USERBOT_VERSION = os.environ.get("USERBOT_VERSION") or "3.6"
+USERBOT_VERSION = os.environ.get("USERBOT_VERSION") or "3.7"
 
 # User Terminal alias
 USER_TERM_ALIAS = os.environ.get("USER_TERM_ALIAS") or "One4uBot"
