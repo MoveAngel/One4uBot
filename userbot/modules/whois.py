251c9416cd428f866e7cbef5e6066759da452147
Mr.Miss <keselekpermen@mrmiss.wtf>
2020-11-21 18:11:54 +0700

    whois: handle anonymous admin
    anonymous admin have no id attributes that make
    the bot crash when fetching. This just handle if
    the user_id return a NoneType.
    
    Co-authored-by: Mr.Miss <keselekpermen@mrmiss.wtf>
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -29,11 +29,14 @@ async def who(event):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
 
     replied_user = await get_user(event)
+    if replied_user is None:
+        await event.edit("`This is anonymous admin in this group.\nCan't fetch the info`")
+        return
 
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
-        event.edit("`Could not fetch info of that user.`")
+        await event.edit("`Could not fetch info of that user.`")
         return
 
     message_id_to_reply = event.message.reply_to_msg_id
@@ -64,7 +67,10 @@ async def get_user(event):
     """ Get the user from argument or replied message. """
     if event.reply_to_msg_id and not event.pattern_match.group(1):
         previous_message = await event.get_reply_message()
-        replied_user = await event.client(GetFullUserRequest(previous_message.from_id))
+        if previous_message.from_id is None:  # Anonymous admin seems don't have id attr
+            return None
+        replied_user = await event.client(
+            GetFullUserRequest(previous_message.from_id))
     else:
         user = event.pattern_match.group(1)
 
@@ -155,6 +161,6 @@ async def fetch_info(replied_user, event):
 CMD_HELP.update(
     {
         "whois": ".whois <username> or reply to someones text with .whois\
-    \nUsage: Gets info of an user."
+        \nUsage: Gets info of an user."
     }
 )
